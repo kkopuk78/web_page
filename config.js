92345a8b2905776a37cfd9765e8e33c47103b783
lucasong78 <lucassong78@bnsr.co.kr>
2026-05-03 21:55:54 +0900

    feat: add Firebase config.js for real-time data

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+window.__firebase_config = JSON.stringify({
+  apiKey: "AIzaSyBpeglet3c24kHrgAYlhNYO3VUJ0e3f1NA",
+  authDomain: "kairos-app-4e86e.firebaseapp.com",
+  projectId: "kairos-app-4e86e",
+  storageBucket: "kairos-app-4e86e.firebasestorage.app",
+  messagingSenderId: "605123534526",
+  appId: "1:605123534526:web:2f096965327f84246748ef",
+  measurementId: "G-43BGLXX7XW"
+});
+window.__app_id = "1:605123534526:web:2f096965327f84246748ef";
